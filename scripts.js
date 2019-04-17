8fe387a1027d7f20bc66878fa92e3ce79fa2fa25
Stefan <stefan.f.miranda@gmail.com>
2019-04-17 15:46:23 -0400

    cleaned up the code

--- scripts.js
+++ scripts.js
@@ -3,94 +3,83 @@ let gridBoxes=10;
 let colorSwitch=false;
 let shadeSwitch=false;
 let boxColors;
+const pushable = document.querySelectorAll(".pushed");
+pushable.forEach(button=>button.addEventListener("click", selectButton))
 
-function generateGrid(boxesNumber){
+function shadeFlip(){
+    if(shadeSwitch){
+        shadeSwitch=false
+    }
+    else{shadeSwitch=true}
+    colorSwitch=false
+}   
+
+function colorFlip(){
+    if(colorSwitch){
+        colorSwitch=false
+    }
+    else{colorSwitch=true}
+    shadeSwitch=false
+}
+
+function generateGrid(){
     container.innerHTML="";
     boxColors={};
+    //create columns that the pixels will be in 
     for(let i=0;i<gridBoxes;i++){
         let divX=document.createElement("div");
-        container.appendChild(divX);
         divX.classList.add("column");
-        
+        container.appendChild(divX);
+        //create the pixels inside each column according to gridboxes
         for (let x = 0; x < gridBoxes; x++) {
-            let divY=document.createElement("div");
-            divY.setAttribute("data-key", `${i}${x}`)
-            divY.classList.add("pixel");
-            divX.appendChild(divY);
+            divX.innerHTML+=`<div class="pixel" data-key="${i}${x}"></div>`
             
-            let pixelColor={};
-            pixelColor["hue"]=0;
-            pixelColor["saturation"]=0;
-            pixelColor["light"]=80;
+            let pixelColor={"hue":0, "saturation":0, "light":80};
             boxColors[`${i}${x}`]=pixelColor;
         }
     }
+
+    //add event listener for each pixel
     const pixels = document.querySelectorAll(".pixel");
     pixels.forEach(pixel => pixel.addEventListener("mouseenter", fillIn)); 
 }
 
-function shade(){
-    shadeSwitch=true;
-    colorSwitch=false;
-}
-
-function color(){
-    colorSwitch=true;
-    shadeSwitch=false;
-}
-
 function setGrid(){
-    const gridSelection=prompt("select number from 2 to 50");
-    if (gridSelection>2 && gridSelection<=50){
-        gridBoxes=gridSelection;
-        reset();
+    gridBoxes=prompt("select number from 2 to 100");
+    if (gridBoxes>2 && gridBoxes<=100){
+        generateGrid();
     }
-}
-
-function reset(){
-    generateGrid()
-    colorSwitch=false;
-    shadeSwitch=false;
+    else{setGrid()}
 }
 
 function fillIn(){
+    //get color info for hovered pixel
     const key=this.dataset.key;
     const box=boxColors[key]
+    let [hue, saturation, light]=[box.hue, box.saturation, box.light]
     
-    let hue=box.hue;
-    let saturation=box.saturation;
-    let light=box.light;
-    
+    //change color info based on which draw type it is set to
     if(colorSwitch){
         light -= 10;
         saturation=100;
         hue=Math.round(Math.random()*360);
-        this.style.background = `hsl(${hue},${saturation}%,${light}%)`
     }
-    else if(shadeSwitch){
-        light-=10;
-        this.style.background = `hsl(${hue},${saturation}%,${light}%)`
-    }
-    else{
-        light=0;
-        this.style.background ="black"
-    }
-    boxColors[key].hue=hue    
-    boxColors[key].saturation=saturation    
-    boxColors[key].light=light    
+    else if(shadeSwitch){light-=10;}
+    else{light=0;}
+
+    //fill in the pixel and update the info of the object
+    this.style.background = `hsl(${hue},${saturation}%,${light}%)`
+    boxColors[key]={"hue":hue,"saturation":saturation,"light":light}
 }
 
 function selectButton(){
     if(this.classList.contains("selected")){    
-        this.classList.remove("selected")
+        this.classList.remove("selected");
     }
     else{
-        pushable.forEach(button=>button.classList.remove("selected"));
+        pushable.forEach(button => button.classList.remove("selected"))
         this.classList.add("selected");
-    };
+    }
 }
-
-const pushable=document.querySelectorAll(".pushed")
-pushable.forEach(button=>button.addEventListener("click", selectButton))
 
 generateGrid();
